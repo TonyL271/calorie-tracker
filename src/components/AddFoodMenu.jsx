89d4a2149b5e0e91383b00e095b84e1e95af52de
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-07-08 17:00:46 -0700

    Remove old designs and files that are no longer being used

---
+++ src/components/AddFoodMenu.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddFoodMenu = () => {
+  return (
+    <div>AddFoodMenu</div>
+  )
+}
+
+export default AddFoodMenu
--- src/components/MealChoice.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const MealChoice = () => {
-  return (
-    <div>MealChoice</div>
-  )
-}
-
-export default MealChoice
