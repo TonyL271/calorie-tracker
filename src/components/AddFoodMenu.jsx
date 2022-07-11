5a632b026f3a280c802ceb06e736081167485610
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-07-11 11:27:37 -0700

    Food nutrients are now responseive to quantity and units placed inside the AddFoodMenu

--- src/components/AddFoodMenu.jsx
+++ src/components/AddFoodMenu.jsx
@@ -9,35 +9,31 @@ const AddFoodMenu = ({ addFood, setAddFood, handleAddFood }) => {
 
 
   const scaleFood = (food) => {
-    // let scaleAmount;
-    // if (selectedUnit === food.serving_unit) {
-    //   scaleAmount = foodQty
-    // } else {
-    //   console.log(food.alt_measures);
-    //   const servingWeight = food.alt_measures.filter(
-    //     (altMeasure) => altMeasure.measure === selectedUnit
-    //   )
-    //   [0];
-    //   let a = 2;
-    //   scaleAmount = foodQty * servingWeight / food.serving_weight.grams
-    // }
-    // let newFoods = { ...food }
-    // const nutrientKeys = Object.entries(food).filter((entry) => entry.substring(0, 2) === "nf")
-    // nutrientKeys.forEach((key) => {
-    //   newFoods[key + '_scaled'] = food[key] * scaleAmount
-    // })
+    let scaleAmount;
+    if (selectedUnit === food.serving_unit) {
+      scaleAmount = foodQty
+    } else {
+      console.log(food.alt_measures);
+      const servingWeight = food.alt_measures.filter(
+        (altMeasure) => altMeasure.measure === selectedUnit
+      )[0];
+      let a = 2;
+      scaleAmount = foodQty * servingWeight.serving_weight / food.serving_weight_grams / servingWeight.qty;
+    }
+    let newFoods = { ...food }
+    let nutrientKeys = Object.entries(food).filter((entry) => entry[0].substring(0, 2) === "nf")
+    nutrientKeys = nutrientKeys.map((entry) => entry[0])
+    nutrientKeys.forEach((key) => {
+      newFoods[key + '_scaled'] = food[key] * scaleAmount
+    })
     return food
   }
 
-
-
   useEffect(() => {
-    // console.log(selectedUnit);
-    // if (selectedUnit.length == 0) {
-    //   setSelectedUnit(foodInfo.serving_unit)
-    // }
+    if (Object.entries(foodInfo).length && !selectedUnit.length) {
+      setSelectedUnit(foodInfo.serving_unit)
+    }
   }, [foodInfo])
-
 
   useEffect(
     () => {
@@ -82,7 +78,7 @@ const AddFoodMenu = ({ addFood, setAddFood, handleAddFood }) => {
           <Typography variant='h3' component="h3">Qnty</Typography>
           <Typography variant='h3' component="h3">Unit</Typography>
           <Box sx={{ width: '3rem', gridColumn: '1/2' }} component="img" src={Object.keys(foodInfo).length > 0 ? foodInfo.photo.thumb : ''} />
-          <Input defaultValue={1} onChange={(e) => setFoodQty(e.currentTarget.value)} />
+          <Input defaultValue={1} onChange={(e) => setFoodQty(parseInt(e.currentTarget.value))} />
           <FormControl fullWidth>
             <InputLabel id="demo-simple-select-label">Unit</InputLabel>
             {foodInfo.alt_measures &&
@@ -90,7 +86,7 @@ const AddFoodMenu = ({ addFood, setAddFood, handleAddFood }) => {
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
                 label="Unit"
-                value={Object.entries(foodInfo).length? foodInfo.serving_unit : ''}
+                value={selectedUnit.length ? selectedUnit : Object.entries(foodInfo).length ? foodInfo.serving_unit : ''}
                 onChange={(e) => { setSelectedUnit(e.target.value) }}
               >
                 {foodInfo.alt_measures.map((elem, idx) => (
