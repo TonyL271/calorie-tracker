4374af2122ab5e0d88a426cdf3678b2a42edbed2
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-08-07 23:53:09 -0700

    Calendar weekends are now greyed out

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -7,7 +7,11 @@ import MealPlan from './MealPlan';
 
 
 const CenteredBox = styled('div')({
-  textAlign: 'center'
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: 'white',
+  fontWeight: '700',
 })
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
@@ -17,21 +21,23 @@ const Calendar = ({ dailyMeals, setDailyMeals }) => {
   const [date, setDate] = useState(new Date());
   const [dates, setDates] = useState([])
   const [firstDay, setFirstDay] = useState(1);
+  const [lastDay, setLastDay] = useState(0);
   const [showDietPlan, setShowDietPlan] = useState({});
 
-  let lastDate;
-
   useEffect(() => {
-    lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-    let firstDay = new Date(date.getFullYear(), date.getMonth());
-    const month = firstDay.getMonth();
-    const year = firstDay.getFullYear();
-    firstDay = firstDay.getDay();
-    lastDate = lastDate.getDate();
+    let lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    let firstDate = new Date(date.getFullYear(), date.getMonth());
+    let firstDay = firstDate.getDay();
+    const month = firstDate.getMonth();
+    const year = firstDate.getFullYear();
+    let lastDay = lastDate.getDay();
+    setLastDay(lastDay)
     setFirstDay(firstDay)
+    console.log('head', firstDay)
+    console.log('tail', 6 - lastDay)
 
     const dates = [];
-    for (let i = 1; i <= lastDate; i += 1) {
+    for (let i = 1; i <= lastDate.getDate(); i += 1) {
       dates.push(new Date(year, month, i))
     }
     setDates(dates)
@@ -39,7 +45,7 @@ const Calendar = ({ dailyMeals, setDailyMeals }) => {
 
   return (
     <Box sx={{ height: '100%', width: '100%' }}>
-      <Box id="calender" sx={{ 'width': '450px', height: '30vh', margin: 'auto' }}>
+      <Box id="calender" sx={{ 'width': '40vw', height: '50vh', margin: 'auto' }}>
         <Box sx={{ mb: '2rem', position: 'relative' }}>
           <CenteredBox id="month">{`${months[date.getMonth()]}  ${date.getFullYear()}`}</CenteredBox>
           <ButtonGroup sx={{ position: 'absolute', top: '0', right: '2rem' }}>
@@ -47,22 +53,32 @@ const Calendar = ({ dailyMeals, setDailyMeals }) => {
             <Button onClick={() => { setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1)) }}>{'>'}</Button>
           </ButtonGroup>
         </Box>
-        <Box id="week-days" sx={{ width: '450px', display: 'grid', gridTemplateColumns: 'repeat(7,1fr)', mb: '1rem' }}>
-          <CenteredBox >Su</CenteredBox>
-          <CenteredBox >Mo</CenteredBox>
-          <CenteredBox >Tu</CenteredBox>
-          <CenteredBox >We</CenteredBox>
-          <CenteredBox >Th</CenteredBox>
-          <CenteredBox >Fr</CenteredBox>
-          <CenteredBox >Sa</CenteredBox>
+        <Box id="week-days"
+          sx={{
+            bgcolor: '#4EDC8E',
+            display: 'grid',
+            gridTemplateColumns: 'repeat(7,1fr)',
+            width: '100%',
+            height: '2rem',
+          }}>
+          <CenteredBox>Su</CenteredBox>
+          <CenteredBox>Mo</CenteredBox>
+          <CenteredBox>Tu</CenteredBox>
+          <CenteredBox>We</CenteredBox>
+          <CenteredBox>Th</CenteredBox>
+          <CenteredBox>Fr</CenteredBox>
+          <CenteredBox sx={{ border: 0 }}>Sa</CenteredBox>
         </Box>
-
-        <Box id="days" sx={{ display: 'grid', gridTemplateColumns: 'repeat(7,1fr)' }}>
-          <Button sx={{ display: firstDay ? 'inline-flex' : 'none', gridColumn: `1 / ${firstDay + 1}`, bgcolor: 'white' }}><time></time></Button>
+        <Box id="days" sx={{ display: 'grid', gridTemplateColumns: 'repeat(7,1fr)', bgcolor: '#50dc8c', gridGap: '2px', }}>
+          {
+            [...Array(firstDay)].map(
+              (elem, idx) => (<Button key={idx} sx={{ display: firstDay ? 'inline-flex' : 'none', bgcolor: idx === 0 ? 'gray' : 'white', borderRadius: 0 }}><time></time></Button>)
+            )
+          }
           {
             dates.map((date, idx) => {
               const match = dailyMeals.filter((meal) => sameDay(meal.getDate(), date));
-              const color = match.length ? 'red' : 'white';
+              const color = match.length ? 'red' : (idx + firstDay) % 7 === 0 || (idx + firstDay) % 7 === 6 ? 'gray' : 'white';
               const onClick = match.length ?
                 () => {
                   setShowDietPlan(match[0])
@@ -70,18 +86,27 @@ const Calendar = ({ dailyMeals, setDailyMeals }) => {
                 () => { };
               return (
                 <Button
-                  sx={{ bgcolor: color, borderRadius: 0 }}
+                  sx={{ bgcolor: color, borderRadius: 0, color: 'black', height: '8rem', display: 'flex', justifyContent: 'end', alignContent: 'start', }}
                   key={idx + 1}
                   onClick={onClick}
                 >
-                  <time>
+                  <Typography sx={{ width: '100%', height: '100%', textAlign: 'end', mr: '0.5rem', mt: '0.5rem' }}>
                     {date.getDate()}
-                  </time>
+                  </Typography>
                 </Button>
               )
             }
             )
           }
+          {
+            [...Array(6 - lastDay)].map(
+              (elem, idx) => {
+                console.log(6-lastDay);
+                return <Button key={idx} sx={{width:'100%',height:'100%',  bgcolor: idx === 5-lastDay ? 'gray' : 'white', borderRadius: 0 }}></Button>
+              }
+            )
+          }
+
         </Box>
         <MealPlan showDietPlan={showDietPlan} setShowDietPlan={setShowDietPlan} />
       </Box>
