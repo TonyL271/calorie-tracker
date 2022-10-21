7fa9c3b98853a982f9a558cb312e14fbeed91327
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-10-20 21:01:42 -0700

    Guest user is created if it is not already in the databse

--- server.js
+++ server.js
@@ -3,8 +3,22 @@ const mongoose = require('mongoose');
 const express = require('express');
 const path = require('path');
 const app = express();
+const UserModel = require('./models/User');
+
 app.use(express.static(path.join(__dirname, './dist')));
 mongoose.connect(process.env.CONNECTION_STRING, { useNewUrlParser: true });
+
+const GuestModel = new UserModel({
+    username: 'guest',
+    password: 'guest',
+    data: []
+})
+GuestModel.register()
+    .then(() => {
+        console.log('guest created');
+    }).catch(err => {
+        console.log(err);
+    });
 
 app.use(express.json());
 app.use('/', require('./routes'));
