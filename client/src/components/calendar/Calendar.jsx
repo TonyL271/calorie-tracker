781c35ea419595250cd7d88e13848774f2b5d949
Tony Liang <tony@Tonys-MacBook-Pro.local>
2022-11-24 08:57:58 -0800

    Completely re-recreated Calendar component for better ui with animations

--- client/src/components/calendar/Calendar.jsx
+++ client/src/components/calendar/Calendar.jsx
@@ -1,148 +1,202 @@
-import { Box, Button, ButtonGroup, Typography } from '@mui/material'
-import { styled } from '@mui/system'
-import { useState, useEffect, useContext, useRef } from 'react';
+import { Box, Typography, Button, IconButton, Paper, Stack, Grow, Zoom, Fade } from "@mui/material";
+import { useEffect, useState, useContext, useReducer, } from "react";
 import MealPlan from './MealPlan';
 import UserContext from '../../context/UserContext';
-import { useLayoutEffect } from 'react';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-const CenteredBox = styled('div')({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  color: 'white',
-  fontWeight: '700',
-  width: '100%'
-})
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const getShowDate = (date) => {
+    date = new Date(date.getFullYear(), date.getMonth(), 1);
+    const isLeapYear = date.getFullYear() % 4 === 0;
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const daysInMonth = [31, isLeapYear ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const daysOfMonth = Array(daysInMonth[month]).fill(0).map((_, i) => new Date(date.getFullYear(), date.getMonth(), i + 1));
+    return ({
+        date,
+        year,
+        month: monthNames[month],
+        daysOfMonth,
+    })
+};
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const sameDay = (date1, date2) => (
+    date1.getDate() === date2.getDate() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getFullYear() === date2.getFullYear()
+)
 
-const sameDay = (date1, date2) =>
-  date1.getDate() === date2.getDate() &&
-  date1.getMonth() === date2.getMonth() &&
-  date1.getFullYear() === date2.getFullYear();
+function Calendar({ dailyMeals, setDailyMeals }) {
+    const [currentDate, setCurrentDate] = useState(new Date());
+    const [showDate, setShowDate] = useState(getShowDate(currentDate));
+    const [showMonth, setShowMonth] = useState(false);
+    const { user, setUser } = useContext(UserContext);
+    const [showDietPlan, setShowDietPlan] = useState({});
 
+    return (
+        <Box display="flex" alignItems="center" justifyContent="center" width="100vw" height="100vh" bgcolor="#f3f9fd" >
+            <Paper sx={{
+                bgcolor: '#fdfdfd',
+                borderRadius: '25px',
+                width: 'max-content',
+                height: 'max-content',
+            }}>
+                <Box height="100%" padding="20px" position="relative">
+                    {/* Header of Calendar */}
+                    <Box display="flex" justifyContent="space-between" alignItems="center" padding="10px"
+                        sx={{
+                        }}>
+                        <Fade timeout={1000} in={!showMonth} key={showDate.month}>
+                            <Button
+                                variant="text"
+                                endIcon={<ExpandMoreIcon sx={{ width: '30px', height: '30px' }} />}
+                                onClick={() => setShowMonth(!showMonth)}
+                                sx={{
+                                    color: 'primary.main',
+                                    padding: '5px 0 5px 10px',
+                                    fontSize: "1.4rem",
+                                    textTransform: "Capitalize",
+                                    borderRadius: "10px",
+                                    '& span': { marginLeft: '0px', marginRight: '0px' }
+                                }}>
+                                {showDate.month}
+                            </Button>
+                        </Fade>
 
-const Calendar = ({ dailyMeals, setDailyMeals }) => {
-  const [date, setDate] = useState(new Date());
-  const [dates, setDates] = useState([])
-  const [firstDay, setFirstDay] = useState(1);
-  const [lastDay, setLastDay] = useState(0);
-  const [showDietPlan, setShowDietPlan] = useState({});
-  const { user, setUser } = useContext(UserContext);
-  const ref = useRef(null);
+                        <Fade timeout={1000} in={!showMonth} key={showDate.year} color="secondary.main" >
+                            <Box display="flex" justifyContent="space-between" alignItems="center" >
+                                <IconButton sx={{ mx: "0px", color: 'secondary.main' }}
+                                    onClick={() => {
+                                        const newDate = new Date(showDate.date);
+                                        newDate.setFullYear(newDate.getFullYear() - 1);
+                                        setShowDate(getShowDate(newDate));
+                                    }
+                                    }>
+                                    <ArrowBackIosNewIcon />
+                                </IconButton>
+                                <Typography variant="h5" component="h5" fontSize="1.4rem" >{showDate.year}</Typography>
+                                <IconButton sx={{ mx: "0px", color: 'secondary.main' }}
+                                    onClick={() => {
+                                        const newDate = new Date(showDate.date);
+                                        newDate.setFullYear(newDate.getFullYear() + 1);
+                                        setShowDate(getShowDate(newDate));
+                                    }}
+                                >
+                                    <ArrowForwardIosIcon />
+                                </IconButton>
+                            </Box>
+                        </Fade>
+                    </Box>
 
-  function useMinDim() {
-    const [size, setSize] = useState(0);
-    useLayoutEffect(() => {
-      function updateSize() {
-        console.log(ref.current.offsetWidth, ref.current.offsetWidth)
-        setSize(Math.min(ref.current.offsetWidth, ref.current.offsetWidth,500));
-      }
-      window.addEventListener('resize', updateSize);
-      updateSize();
-      return () => window.removeEventListener('resize', updateSize);
-    }, []);
-    return size;
-  }
+                    {/* Body of calendar */}
+                    <Box sx={{
+                        display: 'grid',
+                        gridTemplateColumns: 'repeat(7, 1fr)',
+                        padding: '10px',
+                        rowGap: '2px',
+                    }}>
+                        {/* Days of th week */}
+                        {weekDays.map((day) => (
+                            <Box key={day} sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                color: 'secondary.main',
+                                fontSize: {
+                                    smallest: '0.8rem',
+                                    mobile: '0.9rem',
+                                    tablet: '1rem',
+                                },
+                                aspectRatio: '1/1',
+                                fontWeight: 'bold',
+                            }}>{day}</Box>
+                        ))}
+                        {/* Dates of the month */}
+                        {showDate.daysOfMonth.map((date, idx) => {
+                            const isToday = sameDay(currentDate, new Date(date.getFullYear(), date.getMonth(), date.getDate()));
+                            // If user has a meal schedule on this date
+                            const schedule = user ?
+                                user?.dailyMeals?.filter((meal) => sameDay(new Date(meal.date), date)) :
+                                dailyMeals.filter((meal) => sameDay(meal.getDate(), date));
+                            const startingWeek = showDate.date.getDay() + 1;
 
-  const minDim = useMinDim();
-
-
-
-  // Generate corresponding dates for corresponding calender month
-  useEffect(() => {
-    let lastDate = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-    let firstDate = new Date(date.getFullYear(), date.getMonth());
-    let firstDay = firstDate.getDay();
-    const month = firstDate.getMonth();
-    const year = firstDate.getFullYear();
-    let lastDay = lastDate.getDay();
-    setLastDay(lastDay)
-    setFirstDay(firstDay)
-
-    const dates = [];
-    for (let i = 1; i <= lastDate.getDate(); i += 1) {
-      dates.push(new Date(year, month, i))
-    }
-    setDates(dates)
-  }, [date])
-
-  return (
-    <Box ref={ref} sx={{ minHeight: 'calc(100vh - 65px)', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <Box id="calendar" sx={{
-        width: `${minDim}px`,
-        height: `${minDim}px`,
-        bgcolor: 'rgba(0,0,0,0.80)',
-        display: 'flex',
-        flexDirection: 'column',
-      }}>
-        <Box id="calendar-header" sx={{ height: '4rem', position: 'relative', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <CenteredBox id="month">{`${months[date.getMonth()]}  ${date.getFullYear()}`}</CenteredBox>
-          <ButtonGroup sx={{ position: 'absolute', right: '5rem', bgcolor: 'white', }}>
-            <Button sx={{ color: 'black', borderColor: 'red' }} onClick={() => { setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1)) }}>{'<'}</Button>
-            <Button sx={{ color: 'black', borderColor: 'red' }} onClick={() => { setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1)) }}>{'>'}</Button>
-          </ButtonGroup>
-        </Box>
-        <Box id="week-days"
-          sx={{
-            bgcolor: 'primary.main',
-            display: 'grid',
-            gridTemplateColumns: 'repeat(7,1fr)',
-            width: '100%',
-            height: '2rem',
-          }}>
-          <CenteredBox>Su</CenteredBox>
-          <CenteredBox>Mo</CenteredBox>
-          <CenteredBox>Tu</CenteredBox>
-          <CenteredBox>We</CenteredBox>
-          <CenteredBox>Th</CenteredBox>
-          <CenteredBox>Fr</CenteredBox>
-          <CenteredBox sx={{ border: 0 }}>Sa</CenteredBox>
-        </Box>
-        <Box id="days" sx={{ display: 'grid', gridTemplateColumns: 'repeat(7,1fr)', bgcolor: 'primary.main', gridGap: '0.20rem', border: 'solid 3px #4EDC8E', height: '80%' }}>
-          {/* empty padding for when days don't start on sunday */}
-          {[...Array(firstDay)].map(
-            (elem, idx) => (<Button key={idx} sx={{ display: firstDay ? 'inline-flex' : 'none', height: '100%', minHeight: '100%', minWidth: '100%', width: '100%', bgcolor: idx === 0 ? 'gray' : 'white', borderRadius: 0 }}><time></time></Button>)
-          )
-          }
-          {/* day 1 begins here */}
-          {
-            dates.map((date, idx) => {
-              const match = user ?
-                user.dailyMeals.filter((meal) => sameDay(new Date(meal.date), date)) :
-                dailyMeals.filter((meal) => sameDay(meal.getDate(), date));
-              const color = match.length ? '#e6008a' : (idx + firstDay) % 7 === 0 || (idx + firstDay) % 7 === 6 ? 'gray' : 'white';
-              const onClick = match.length ?
-                () => {
-                  setShowDietPlan(match[0])
-                } :
-                () => { };
-              return (
-                <Button
-                  sx={{ bgcolor: color, borderRadius: 0, color: 'black', minHeight: '100%', height: '100%', minWidth: '100%', width: '100%', display: 'flex', justifyContent: 'end', alignContent: 'start', }}
-                  key={idx + 1}
-                  onClick={onClick}
-                >
-                  <Typography sx={{ width: '100%', height: '100%', textAlign: 'end', mr: '0.5rem', mt: '0.5rem' }}>
-                    {date.getDate()}
-                  </Typography>
-                </Button>
-              )
-            })
-          }
-          {/* padding for when days don't end on saturday */}
-          {
-            [...Array(6 - lastDay)].map(
-              (elem, idx) => {
-                return <Button key={idx} sx={{ minWidth: '100%', width: '100%', height: '100%', bgcolor: idx === 5 - lastDay ? 'gray' : 'white', borderRadius: 0 }}></Button>
-              }
-            )
-          }
-        </Box>
-        <MealPlan showDietPlan={showDietPlan} setShowDietPlan={setShowDietPlan} />
-      </Box>
-    </Box>
-  )
+                            return (
+                                <Grow in={!showMonth} key={showDate.year + showDate.daysOfMonth + idx} timeout={1000} style={{
+                                    transformOrigin: '0 0 0'
+                                }}>
+                                    <Box sx={{
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                        width: '40px',
+                                        minWidth: '40px',
+                                        minHeight: '40px',
+                                        color: 'black',
+                                        fontSize: {
+                                            smallest: '0.8rem',
+                                            mobile: '0.9rem',
+                                            tablet: '1rem',
+                                        },
+                                        aspectRatio: '1/1',
+                                        borderRadius: '50%',
+                                        bgcolor: schedule.length ? 'secondary.main' : 'rgba(0,0,0,0)',
+                                        fontWeight: isToday ? '900' : 'normal',
+                                        gridColumn: idx === 0 ? `${startingWeek}` : 'auto',
+                                    }}
+                                        onClick={() => { schedule.length && setShowDietPlan(schedule[0]); }}
+                                    >
+                                        {date.getDate()}
+                                    </Box>
+                                </Grow>
+                            )
+                        })}
+                    </Box>
+                    {/* Select Month menu */}
+                    <Paper sx={{
+                        visibility: showMonth ? 'visible' : 'hidden',
+                        position: 'absolute',
+                        padding: '10px',
+                        bgcolor: '#fdfdfd',
+                        borderRadius: '25px',
+                        width: '100%',
+                        height: '100%',
+                        top: 0,
+                        left: 0,
+                        display: 'grid',
+                        gridTemplateColumns: 'repeat(3, auto)',
+                    }}>
+                        {monthNames.map((month, idx) => (
+                            <Zoom key={month} in={showMonth} timeout={500}>
+                                <Stack justifyContent="center">
+                                    <Button
+                                        onClick={() => {
+                                            setShowDate(getShowDate(new Date(showDate.year, idx, 1)));
+                                            setShowMonth(false);
+                                        }}
+                                        sx={{
+                                            display: 'flex',
+                                            justifyContent: 'center',
+                                            alignItems: 'center',
+                                            fontSize: '1.0rem',
+                                            borderRadius: '10px',
+                                            px: '5px',
+                                            py: '3px',
+                                            textTransform: 'Capitalize',
+                                        }}>
+                                        {month}
+                                    </Button>
+                                </Stack>
+                            </Zoom>
+                        ))}
+                    </Paper>
+                </Box>
+                <MealPlan showDietPlan={showDietPlan} setShowDietPlan={setShowDietPlan} />
+            </Paper >
+        </Box >
+    );
 }
 
-export default Calendar
+export default Calendar;
